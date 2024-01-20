b973f88f7cea3e4e31ebaaed68feaf4efa73200d
Jyothish-Kumar-S-D <jyothish2004jksd@gmail.com>
2024-01-20 18:39:45 +0530

    initial commit files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import NavBar from './components/navbar.jsx';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div>
       <Home />
+      <NavBar />
     </div>
   );
 }
