0c33986bd797cea834872e7186beb580d993af3b
Atulya Raghuvanshi <atulyaraghuvanshi@gmail.com>
2024-01-20 18:38:22 +0530

    Navbar.css added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './styles/Navbar.css' ;
 
-const Home = () => {
+function Home() {
     return (
         <div>
             Home
         </div>
-    )
+    );
 }
 
 export default Home
