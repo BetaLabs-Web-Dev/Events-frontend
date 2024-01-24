8b4dfb055131699fef7e2a1274078e0dea4feb75
Ankur7891 <ankurmajumdar7891@gmail.com>
2024-01-24 17:11:57 +0530

    Module CSS for BG + Assets Folder in Src (Image as SVG)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Background from './components/Background/Background';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div>
+      <Background />
       <Home />
     </div>
   );
---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Background.module.css';
+
+function Background() {
+  return (
+    <div className={styles.Background}></div>
+  )
+}
+
+export default Background;
