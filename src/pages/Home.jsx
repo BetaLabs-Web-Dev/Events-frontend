57eab55d2f4e2459841cc3a577445a1d15766b89
RamKishore <mramki1803@gmail.com>
2024-01-29 23:31:34 +0530

    Navbar and Hero (First commit)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import Navbar from "../components/NavBar";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div>
-      <NavBar />
+      <Navbar />
+      <Hero />
     </div>
   );
 };
